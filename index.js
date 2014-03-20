5f6f819e985907c37f88db7ab33946e3bb14435f
Hsiaoming Yang <me@lepture.com>
2014-03-20 12:31:47 +0800

    Support triple em dash. Don't add extra <p> tag.

--- index.js
+++ index.js
@@ -6,20 +6,22 @@ module.exports = function(html) {
   // remove too many white space
   html = html.replace(/&nbsp;?/g, ' ');
 
-  // transfer *** to <hr>
-  html = html.replace(/^\s*(?:\*\s*){3,}\s*$/gm, '<hr>');
+  // transfer *** --- ——— to <hr>
+  html = html.replace(/^\s*(?:(?:\*|\-|—)\s*){3,}\s*$/gm, '<hr>');
 
-  // transfer --- to <hr>
-  html = html.replace(/^\s*(?:\-\s*){3,}\s*$/gm, '<hr>');
+  html = html.replace(/<p>(?:(?:\*|\-|—)\s*){3,}</g, '<p><hr><');
+  html = html.replace(/<br\s*\/?>(?:(?:\*|\-|—)\s*){3,}</g, '<hr><');
 
   // transfer -- to ——
   html = html.replace(/([^-])(--)([^-])/g, '$1——$3');
 
   // transfer <br> to <p>
   var snippets = html.split(/(?:<br\s*\/?>[\s|\n]*){2,}/);
-  html = '';
-  for (var i = 0; i < snippets.length; i++) {
-    html += '<p>' + snippets[i] + '</p>';
+  if (snippets.length > 1) {
+    html = '';
+    for (var i = 0; i < snippets.length; i++) {
+      html += '<p>' + snippets[i] + '</p>';
+    }
   }
   return html;
 };
--- test/spec.js
+++ test/spec.js
@@ -10,14 +10,14 @@ describe('pretty', function() {
 
   it('should remove useless spaces', function() {
     var text = 'foo &nbsp; &nbsp bar';
-    assert(pretty(text), '<p>foo     bar</p>');
+    assert(pretty(text), 'foo     bar');
   });
 
   it('should transform --- to hr', function() {
     var text = 'foo ---';
-    assert(pretty(text), '<p>foo ---</p>');
+    assert(pretty(text), 'foo ---');
 
-    var expect = '<p>foo\n<hr></p>';
+    var expect = 'foo\n<hr>';
     text = 'foo\n ---';
     assert(pretty(text), expect);
 
@@ -27,9 +27,9 @@ describe('pretty', function() {
 
   it('should transform *** to hr', function() {
     var text = 'foo ***';
-    assert(pretty(text), '<p>foo ***</p>');
+    assert(pretty(text), 'foo ***');
 
-    var expect = '<p>foo\n<hr></p>';
+    var expect = 'foo\n<hr>';
     text = 'foo\n ***';
     assert(pretty(text), expect);
 
@@ -37,9 +37,15 @@ describe('pretty', function() {
     assert(pretty(text), expect);
   });
 
+  it('should transform to hr', function() {
+    var text = '<br>———————————————————————<br>';
+    var expect = '<hr><br>';
+    assert(pretty(text), expect);
+  });
+
   it('should transfer minus to dash', function() {
     var text = 'foo -- bar';
-    assert(pretty(text), '<p>foo —— bar</p>');
+    assert(pretty(text), 'foo —— bar');
   });
 
   it('should remove too many br', function() {
